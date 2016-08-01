5b6f4a4587e3649e42dcec1041e0f98c14d5d0b7
MATT BRUTON <the.bruton@gmail.com>
2016-08-01 13:42:31 -0500

    created initial files

---
+++ NSS-Alphabits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NssAlphabits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User testSubject = new NssAlphabits.User("XB102108");
+            Console.WriteLine(testSubject.Greeting());
+            testSubject.GiveUserHumanName(Console.ReadLine());
+            Console.WriteLine(testSubject.UserName);
+            Console.WriteLine(testSubject.Congratulate());
+        }
+    }
+}
---
+++ NSS-Alphabits/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NssAlphabits
+{
+    public class User
+    {
+        public User(string name)
+        {
+            UserName = name;
+        }
+        public string UserName { get; private set; }
+        public void GiveUserHumanName(string name)
+        {
+            UserName = name;
+        }
+        public string Greeting()
+        {
+            return String.Format("Human, we need your letters!\n\nIf you want to see your family again, write all of them!\n\nRecord your name, letterslave:");
+        }
+        public string Congratulate()
+        {
+            return String.Format("Thanks, here is some sludgefood. Tomorrow, we need your numbers.\nCongratulations, {0}!", UserName);
+        }
+        public void AddChar()
+        {
+
+        }
+    }
+}
