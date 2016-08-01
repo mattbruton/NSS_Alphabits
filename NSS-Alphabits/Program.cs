5fd00c9423fbc7cf5d2c270d371e55d385ee1419
MATT BRUTON <the.bruton@gmail.com>
2016-08-01 14:48:06 -0500

    added alien ascii image, added method in user to steal letters from name to add to list

--- NSS-Alphabits/Program.cs
+++ NSS-Alphabits/Program.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NssAlphabits.Images;
 
 namespace NssAlphabits
 {
     class Program
     {
+        static string Prompt()
+        {
+            return String.Format("> ");
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Image.AlienImage());
             User testSubject = new NssAlphabits.User("XB102108");
             Console.WriteLine(testSubject.Greeting());
+            Console.Write(Prompt());
             testSubject.GiveUserHumanName(Console.ReadLine());
             Console.WriteLine(testSubject.UserName);
             Console.WriteLine(testSubject.Congratulate());
--- NSS-Alphabits/User.cs
+++ NSS-Alphabits/User.cs
@@ -9,20 +9,33 @@ namespace NssAlphabits
         {
             UserName = name;
         }
+
         public string UserName { get; private set; }
+
         public void GiveUserHumanName(string name)
         {
             UserName = name;
         }
+
         public string Greeting()
         {
             return String.Format("Human, we need your letters!\n\nIf you want to see your family again, write all of them!\n\nRecord your name, letterslave:");
         }
+
         public string Congratulate()
         {
             return String.Format("Thanks, here is some sludgefood. Tomorrow, we need your numbers.\nCongratulations, {0}!", UserName);
         }
-        public void AddChar()
+
+        public void StealNameLetters()
+        {
+            foreach (char letter in UserName)
+            {
+                AddChar(letter);
+            }
+        }
+
+        public void AddChar(char letter)
         {
 
         }
